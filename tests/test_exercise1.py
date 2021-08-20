9c80af3b950267cb8763fa6cff2323ee18dc84ca
Angelica <Angelica137@users.noreply.github.com>
2021-08-20 22:09:21 +0100

    .string_reverse() test3, green

--- tests/test_exercise1.py
+++ tests/test_exercise1.py
@@ -8,3 +8,8 @@ def test_string_reverser_returns_retaw():
 def test_string_reverse_returns_test2():
     string = 'Practicing string manipulation!'
     assert string_reverser(string) == '!noitalupinam gnirts gnicitcarP'
+
+
+def test_string_reverse_returns_test3():
+    string = 'The house code is: 2343'
+    assert string_reverser(string) == '3432 :si edoc esuoh ehT'
