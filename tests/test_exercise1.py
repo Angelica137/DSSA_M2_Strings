5c488aa9874db11992b946123c58b9538041432f
Angelica <Angelica137@users.noreply.github.com>
2021-08-20 22:08:27 +0100

    .string_reverse() test2, green

--- tests/test_exercise1.py
+++ tests/test_exercise1.py
@@ -1,5 +1,10 @@
 from scripts.Exercise1 import string_reverser
 
 
-def test_string_reverser_returns_string():
+def test_string_reverser_returns_retaw():
     assert string_reverser('water') == 'retaw'
+
+
+def test_string_reverse_returns_test2():
+    string = 'Practicing string manipulation!'
+    assert string_reverser(string) == '!noitalupinam gnirts gnicitcarP'
