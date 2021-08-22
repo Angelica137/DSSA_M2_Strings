d9853a7677233f7bb46eeb10e698ef8ac85eeea3
Angelica <Angelica137@users.noreply.github.com>
2021-08-22 11:26:11 +0100

    anagram_checker tests string length test2, green

--- tests/test_exercise2.py
+++ tests/test_exercise2.py
@@ -3,3 +3,7 @@ from scripts.Exercise2 import anagram_checker
 
 def test_anagram_checker_length_differ():
     assert anagram_checker('lol', 'lolz') == False
+
+
+def test_anagram_length_differ_2():
+    assert anagram_checker('water', 'waiter') == False
