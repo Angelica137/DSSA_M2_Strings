b54600a055f3aa59d3c121f8044cc79386e0ad9a
Angelica <Angelica137@users.noreply.github.com>
2021-08-22 18:53:06 +0100

    .word_flipper() returns reverse 2 word string, red

--- tests/test_exercise3.py
+++ tests/test_exercise3.py
@@ -3,3 +3,7 @@ from scripts.Exercise3 import word_flipper
 
 def test_word_flipper_returns_reverse_string():
     assert word_flipper('hello') == 'olleh'
+
+
+def test_world_flipper_returns_reverse_string_2_words():
+    assert word_flipper('hello goodbye') == 'olled eybdoog'
